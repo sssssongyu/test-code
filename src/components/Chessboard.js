faef490c8c7f39c135498e2c860ac81cae0d6bc9
Yu song <c7ls32@gmail.com>
2025-08-12 11:03:25 +1000

    interview by song

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -8,10 +8,41 @@ const indexToLetter = (index) => {
 };
 
 const Chessboard = () => {
+  const size = 8
+  const line = Array(size).fill(1)
+  const column = Array(size).fill(1)
+  const string = ['a', 'b', 'c', 'd', 'e', 'f', 'g', '']
+
+  const returnClassName = (columnI, LineI) => {
+    let res = ''
+    if ((columnI + LineI) % 2 === 1) {
+      res = 'greenblock'
+    } else {
+      res = 'yellowblock'
+    }
+    if ((columnI === 0 && LineI === 0) || (columnI === size-1 && LineI === size-1)) {
+      res += ' redborder'
+    }
+    return res
+  }
   return (
     <div>
-      TODO: Build the chessboard in React + CSS here Build the chessboard in
-      React + CSS here Build the chessboard in React + CSS here
+      <div>
+        TODO: Build the chessboard in React + CSS here Build the chessboard in
+        React + CSS here Build the chessboard in React + CSS here
+      </div>
+      <div>
+        {line.map((d, lineIndex) => (
+          <div key={lineIndex + 'line'} className="line">
+            {column.map((c, columnIndex) => (
+              <div key={columnIndex + 'column'} className={returnClassName(columnIndex, lineIndex)}>
+                {columnIndex === 0 && lineIndex !== 0 && <div className='numberPosition'>{size - lineIndex}</div>}
+                {lineIndex === size-1 && <div className='stringPosition'>{string[columnIndex]}</div>}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
